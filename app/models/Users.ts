442348bebaa0cb327b3e7461d3c18ca1be81ea39
Gary Smith <garysmith193358@gmail.com>
2024-09-12 19:50:06 -0400

    created Users.ts to create User model for database

---
+++ app/models/Users.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface User extends Document {
+  name: string,
+  email: string,
+  password: string;
+}
+
+const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String, 
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model<User>("User", userSchema);
+
+export default User;
