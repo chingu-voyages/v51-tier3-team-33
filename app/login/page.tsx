a359a7cab238a9db9b2df0d8d3419cfa244efaef
Jaweki <werukioni@gmail.com>
2024-09-14 15:06:43 +0300

    modified the root page.tsx and added the /protected route; for test purposes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,11 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 const Page = () => {
   return (
     <div>
       <Button onClick={() => signIn("google")}> Login with google</Button>
+      <Link href={"/"}> Go to home</Link>
     </div>
   );
 };
---
+++ app/protected/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+
+const ProtectedPage = () => {
+  const { data } = useSession();
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-3">
+      This is a Protected route
+      <div className=" flex flex-col items-center p-1 border rounded-md">
+        <Image
+          src={data?.user?.image || ""}
+          alt="USER AVATAR"
+          width={120}
+          height={120}
+        />
+        <span>{data?.user?.name}</span>
+        <span>{data?.user?.email}</span>
+      </div>
+      <button onClick={() => signOut()}>Sign Out</button>
+    </div>
+  );
+};
+
+export default ProtectedPage;
