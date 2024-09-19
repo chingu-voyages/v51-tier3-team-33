4e17c7853167139dbe79985fe3d497e773f76b38
Gary Smith <garysmith193358@gmail.com>
2024-09-19 15:33:23 -0400

    Created Receipt.ts to add receipt field to database

---
+++ models/Receipt.ts
@@ -0,0 +1,16 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface Receipt extends Document { 
+  image_url: String
+}
+
+const receiptSchema: Schema = new Schema({
+  image_url: {
+    type: String,
+    required: true
+  }
+});
+
+const Receipt = mongoose.model<Receipt>("Receipt", receiptSchema);
+
+export default Receipt;
