2eec855d865a282cdb5917232e47fdcec7e6f125
Olga Yudkin <cvtqx@protonmail.com>
2024-09-20 13:42:09 -0400

    cancel button in dialog

--- app/(dashboard)/ui/dialogPopup.tsx
+++ app/(dashboard)/ui/dialogPopup.tsx
@@ -1,14 +1,17 @@
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import { DialogTitle } from '@radix-ui/react-dialog';
 
 export function DialogPopup() {
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -16,18 +19,22 @@ export function DialogPopup() {
           Log out
         </Button>
       </DialogTrigger>
-      <DialogContent className='md:max-w-[425px] flex flex-col items-center rounded'>
+      <DialogContent className='md:max-w-[425px] flex flex-col items-center rounded bg-white'>
         <DialogHeader>
+          <DialogTitle></DialogTitle>
           <DialogDescription className='text-center'>
             Are you sure you want to log out?
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className='flex flex-row justify-center gap-4'>
-          <Button
-            type='button'
-            className='bg-red outline-none text-white font-semibold hover:bg-paleRed hover:text-red'>
-            Cancel
-          </Button>
+          <DialogClose asChild>
+            <Button
+              type='button'
+              variant='secondary'
+              className='bg-red outline-none text-white font-semibold hover:bg-paleRed hover:text-red'>
+              Cancel
+            </Button>
+          </DialogClose>
           <Button
             type='button'
             className='bg-purple outline-none text-white font-semibold hover:bg-solitude hover:text-purple'>
