7c184e71dcbe22d6516ed8e0c8a257eaae9a3ee3
Olga Yudkin <cvtqx@protonmail.com>
2024-09-20 15:26:01 -0400

    ui components for homepage

---
+++ app/(dashboard)/home/ui/recentActivity.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Separator } from '@/components/ui/separator';
+
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
+);
+
+export default function RecentActivity() {
+    return (
+      <div>
+        <h2 className='mb-2'>Recent activity:</h2>
+        <ScrollArea className='h-64 w-full md:w-[250px] rounded-md border bg-muted'>
+          <div className='p-4'>
+            {tags.map((tag) => (
+              <>
+                <div
+                  key={tag}
+                  className='text-sm'>
+                  {tag}
+                </div>
+                <Separator className='my-2' />
+              </>
+            ))}
+          </div>
+        </ScrollArea>
+      </div>
+    );
+}
