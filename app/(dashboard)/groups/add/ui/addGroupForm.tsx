b9fc551c6220da4150e30173f120618470bbbb1e
Olga Yudkin <cvtqx@protonmail.com>
2024-10-04 13:44:33 -0400

    added adduser popover and user search function

--- app/(dashboard)/groups/add/ui/addGroupForm.tsx
+++ app/(dashboard)/groups/add/ui/addGroupForm.tsx
@@ -1,18 +1,10 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { DatePicker } from '../../ui/DatePicker';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
 import { useForm } from 'react-hook-form';
+import AddNewMemberForm from './addNewMemberForm';
 
 //fake data generator - remove after connecting to db
 import { generateFakeMembers } from '@/utils/generateFakeMembers';
@@ -27,77 +19,68 @@ interface Member {
 const fakeMembers = generateFakeMembers(3);
 
 const NewGroupForm: React.FC = () => {
-  const { register, reset } = useForm();
+  const { register, handleSubmit, reset, setValue } = useForm();
 
   const [members, setMembers] = useState<Member[]>([]);
-
+  const [groupType, setGroupType] = useState<string | null>(null);
   //remove this useeffect and fakemembers when db is connected
 
   useEffect(() => {
     setMembers(fakeMembers);
   }, []);
 
+  const onSubmit = (data: any) => {
+    console.log('From data:', data);
+    reset();
+  };
+
+  const handleGroupTypeSelect = (type: string) => {
+    setGroupType(type);
+    setValue('groupType', type);
+  };
+
+  const handleDeleteMember = (id: number) => {
+    setMembers((prev) => prev.filter((member) => member.id !== id));
+  };
+
   return (
-    <form className='flex flex-col p-5'>
+    <form
+      className='flex flex-col p-5'
+      onSubmit={handleSubmit(onSubmit)}>
       <h1 className='text-2xl font-bold mb-4'>New Group</h1>
       <div className='mb-4'>
-        <h2 className='text-lg mb-2'>Type</h2>
+        <h2 className='text-lg mb-2'>Group Type</h2>
         <div className='flex gap-2'>
-          <Button
-            type='button'
-            className='bg-green-300 text-white'
-            value='trip'>
-            Trip
-          </Button>
-          <Button
-            type='button'
-            className='bg-green-300 text-white'
-            value='party'>
-            Party
-          </Button>
-          <Button
-            type='button'
-            className='bg-green-300 text-white'
-            value='dining'>
-            Dining
-          </Button>
-          <Button
-            type='button'
-            className='bg-green-300 text-white'
-            value='housing'>
-            Housing
-          </Button>
-          <Button
-            type='button'
-            className='bg-green-300 text-white'
-            value='other'>
-            Other
-          </Button>
+          {['trip', 'party', 'dining', 'housing', 'other'].map((type) => (
+            <Button
+              type='button'
+              className={`bg-green-300 text-white ${
+                groupType === type ? 'font-bold bg-black' : ''
+              }`}
+              key={type}
+              onClick={() => handleGroupTypeSelect(type)}>
+              {type.toUpperCase()}
+            </Button>
+          ))}
         </div>
         <div className='my-4'>
-          <label className='block  text-lg mb-2'>Group Name</label>
+          <label className='block text-lg mb-2'>Group Name</label>
           <Input
             {...register('groupName', { required: true })}
             placeholder='Enter group name'
+            type='text'
+            name='groupName'
+            maxLength={50}
           />
         </div>
-        <div className='mb-4'>
-          <label className='block text-lg mb-2'>Date</label>
-          <DatePicker {...register('date')} />
-        </div>
-        <div className='mb-4'>
-          <label className='block mb-2'>Currency</label>
-          <Select {...register('currency')}>
-            <SelectTrigger className='w-[180px]'>
-              <SelectValue placeholder='Select a currency' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value='USD'>USD</SelectItem>
-                <SelectItem value='EUR'>EUR</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+        <div className='my-4'>
+          <label className='block  text-lg mb-2'>Group Budget</label>
+          <Input
+            {...register('groupBudget', { required: true })}
+            placeholder='Enter group budget'
+            type='number'
+            name='groupBudget'
+          />
         </div>
         <div className='mb-4 flex flex-col'>
           <h2 className='text-lg font-semibold mb-2'>Members</h2>
@@ -106,7 +89,7 @@ const NewGroupForm: React.FC = () => {
           )}
           {members.map((member) => (
             <div
-              key={member.name}
+              key={member.id}
               className='flex items-center justify-between border bg-muted'>
               <div className='ml-2'>
                 <p className='font-semibold'>{member.name}</p>
@@ -115,17 +98,15 @@ const NewGroupForm: React.FC = () => {
               <div>
                 <Button
                   type='button'
-                  className='bg-red mr-2'>
+                  className='bg-red mr-2'
+                  onClick={() => handleDeleteMember(member.id)}>
                   Delete
                 </Button>
               </div>
             </div>
           ))}
-          <Button
-            type='button'
-            className='bg-green-300 text-white mt-4 w-64 mx-auto block'>
-            Add Group Member
-          </Button>
+          <AddNewMemberForm />
+
         </div>
         <div className='flex justify-center gap-4 mt-4'>
           <Button
---
+++ app/(dashboard)/groups/add/ui/addNewMemberForm.tsx
@@ -0,0 +1,109 @@
+//search users collection based on input value and returna  friend
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Search } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+interface User {
+  _id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export default function AddNewMemberForm() {
+  const [searchQuery, setSearchQuery] = useState<string | null>(null);
+  const [searchResults, setSearchResults] = useState<User[]>([]);
+  const [hasStartedSearch, setHasStartedSearch] = useState<boolean>(false);
+
+  useEffect(() => {
+    setSearchResults([]);
+    setSearchQuery(null);
+    setHasStartedSearch(false);
+  }, []);
+
+  const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    setHasStartedSearch(true);
+
+    if (query) {
+      try {
+        const response = await fetch('/api/users');
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+        const data = await response.json();
+          
+          const foundUsers = data.users;
+
+          const filtered = foundUsers.filter(
+            (user) =>
+              user.firstName.toLowerCase().includes(query.toLowerCase()) ||
+              user.lastName.toLowerCase().includes(query.toLowerCase()) ||
+              user.email.toLowerCase().includes(query.toLowerCase())
+          );
+          setSearchResults(filtered);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    } else {
+      setSearchResults([]);
+    }
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className='bg-green-300 text-white mt-4 w-64 mx-auto block'>
+          Add New Member
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-80'>
+        <div className='flex flex-col gap-4'>
+          <form>
+            <div className='relative'>
+              <Search className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
+              <Input
+                type='search'
+                placeholder='Enter name or email...'
+                className='w-full appearance-none bg-background pl-8 shadow-none'
+                onChange={handleSearch}
+              />
+            </div>
+            <div className='mt-4'>
+              {hasStartedSearch && searchResults.length === 0 ? (
+                <p>No results found</p>
+              ) : (
+                searchResults.length > 0 && (
+                  <ul>
+                    {searchResults.map((user) => (
+                      <li
+                        key={user._id}
+                        className='flex justify-between items-center py-2 border-b'>
+                        <span>
+                          {user.firstName} {user.lastName} ({user.email})
+                        </span>
+                        <Button
+                          type='button'
+                          className='bg-purple text-white mt-4'>
+                          Add
+                        </Button>
+                      </li>
+                    ))}
+                  </ul>
+                )
+              )}
+            </div>
+          </form>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
