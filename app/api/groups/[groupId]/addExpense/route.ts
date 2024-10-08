8436fe9efa9de9115d5ef357c8a4f9c518c03525
Gary Smith <garysmith193358@gmail.com>
2024-10-08 16:37:26 -0400

    Adjusted addExpense api to handle addition of user contributions

--- app/api/groups/[groupId]/addExpense/route.ts
+++ app/api/groups/[groupId]/addExpense/route.ts
@@ -4,16 +4,21 @@ import Expense from "@/models/Expenses";
 import UserExpense from "@/models/UserExpense";
 import Group from "@/models/Group";
 
+interface Contributions { // have the key be the userid and value be the amount of the contribution?
+  member_id: string;
+  amount: number;
+}
+
 interface ExpenseBody {
   name: string;
   description: string;
   amount: number;
   category: string
   receiptFile?: File
-  //contributions will go here
+  member_contributions: Contributions[];
 }
 
-export const POST = async(request:NextRequest, { params } : { params: { groupId: string } }): Promise<any> => {
+export const POST = async(request:NextRequest, { params } : { params: { groupId: string } }): Promise<NextResponse> => {
   try {
     await dbConnect();
 
@@ -27,23 +32,14 @@ export const POST = async(request:NextRequest, { params } : { params: { groupId:
       amount: parseFloat(formData.get("amount") as string),
       category: formData.get("category") as string,
       receiptFile: formData.get("file") as File,
+      member_contributions: JSON.parse(formData.get("contributions") as string) //make sure everything excluding the value of the amount are in double quotes like this: { "member_id": "id", "amount": 50}
     };
-
-    let expense = await Expense.create({
-      name: body.name,
-      description: body.description,
-      amount: body.amount,
-      category: body.category,
-      group_id: groupId,
-      receipt_url: receiptUrl // initially undefined.
-    })
 
     if (body.receiptFile) {
       const receiptFormData = new FormData();
       receiptFormData.append("file", body.receiptFile);
-      receiptFormData.append("expenseId", (expense._id) as string); // passing the expense id to the form to easily identify the expense the image will belong to in AWS S3
 
-      const receiptRequest = await fetch(`${process.env.BASE_URL}/api/groups/s3-upload`,{
+      const receiptRequest = await fetch(`${process.env.BASE_URL}/api/s3-upload`,{
         method: "POST",
         body: receiptFormData
       });
@@ -51,35 +47,38 @@ export const POST = async(request:NextRequest, { params } : { params: { groupId:
       if (receiptRequest.ok) {
         const receiptData = await receiptRequest.json();
         receiptUrl = receiptData.storedReceiptUrl;
-
-        const updatedExpense = await Expense.findByIdAndUpdate(expense._id, // add the receipt url to the newly created expense
-          {receipt_url: receiptUrl}, {new: true}
-        )
-
-        if (updatedExpense) { // reassign the expense to be returned with the new expense that has the receipt url
-          expense = updatedExpense;
-        }
       } else {
         throw new Error('Failed to upload receipt');
       }
     }
 
+    const expense = await Expense.create({
+      name: body.name,
+      description: body.description,
+      amount: body.amount,
+      category: body.category,
+      group_id: groupId,
+      receipt_url: receiptUrl
+    })
+
     const updatedGroup = await Group.findByIdAndUpdate(groupId,
       { $addToSet: {expenses: expense._id} }, // add to set prevent duplicates from being added.
       { new: true }) // returns the new verison of the document instead of the old one
 
-    if (updatedGroup?.members && updatedGroup.members.length > 0) {
-      for (const memberId of updatedGroup.members) {
+    const hasMembers = updatedGroup?.members && updatedGroup.members.length > 0;
+    const hasContributions = body.member_contributions.length > 0;
+  
+    if (hasMembers && hasContributions) {
+      for (const contribution of body.member_contributions) {
         const userExpense = await UserExpense.create({
-          user_id: memberId,
+          user_id: contribution.member_id,
           expense_id: expense.id,
-          //contributions still need to be added
+          contribution: contribution.amount
         })
-
+  
         console.log(userExpense);
       }
     }
-    //may still need the user_id to verify that one creating the expense is the admin. - ask the group
 
     return NextResponse.json({ message: 'Expense created successfully', expense, updatedGroup }, { status: 201 });
 
