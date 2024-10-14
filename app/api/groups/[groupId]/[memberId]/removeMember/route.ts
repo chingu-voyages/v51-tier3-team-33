4b8236c6525d39daa965b26eb3152b89fdf4dc79
Gary Smith <garysmith193358@gmail.com>
2024-10-14 17:50:48 -0400

    moved memberId to be passed through url instead of request body

--- app/api/groups/[groupId]/[memberId]/removeMember/route.ts
+++ app/api/groups/[groupId]/[memberId]/removeMember/route.ts
@@ -4,17 +4,16 @@ import dbConnect from "@/lib/dbConnect";
 import User from "@/models/Users";
 import UserExpense from "@/models/UserExpense";
 import UserGroup from "@/models/UserGroup";
+import { ObjectId } from "mongoose";
 
 
-export const DELETE = async (request: NextRequest, { params }: { params: { groupId: string } }) => {
+export const DELETE = async (request: NextRequest, { params }: { params: { groupId: string, memberId: ObjectId} }) => {
   try {
     await dbConnect();
 
-    const { groupId } = params;
-    const body = await request.json();
-    const { userId } = body
+    const { groupId, memberId } = params;
 
-    const userExists = await User.findById(userId)
+    const userExists = await User.findById(memberId)
 
     if (!userExists) {
       return NextResponse.json({error: "No user with that id"}, {status: 404})
@@ -26,21 +25,21 @@ export const DELETE = async (request: NextRequest, { params }: { params: { group
       return NextResponse.json({error: "No group with that id"}, {status: 404})
     }
 
-    const userInGroup = group.members.includes(userId)
+    const userInGroup = group.members.includes(memberId)
 
     if (!userInGroup) {
       return NextResponse.json({error: "The user is not in apart of this group"}, {status: 400})
     }
 
-    group = await Group.findByIdAndUpdate(groupId, { //remove the id of the user from the members array.
-      $pull: { members: userId }
+    group = await Group.findByIdAndUpdate(groupId, { //removes the id of the user from the members array.
+      $pull: { members: memberId }
     }, { new: true })
 
-    const userGroup = await UserGroup.findOneAndDelete({ group_id: groupId, user_id: userId }); //delete the UserGroup record.
+    const userGroup = await UserGroup.findOneAndDelete({ group_id: groupId, user_id: memberId }); // deletes the UserGroup record.
     console.log("user group", userGroup)
 
     const expenses = await UserExpense.deleteMany({ // removes all user expenses record that is related to expenses in the current group.
-      user_id: userId,
+      user_id: memberId,
       expense_id: { $in: group!.expenses }
     });
 
