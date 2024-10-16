23bc52e96cb8eb96616972ae650198b69f854e32
Gary Smith <garysmith193358@gmail.com>
2024-10-16 07:54:52 -0400

    Adjusted Auth/User route to add 5 random friends upon signup for testing purposes

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -17,12 +17,25 @@ export const POST = async(request: NextRequest) => {
       return NextResponse.json({message: "Email is missing"}, {status:400});
     }
 
+    const friends = await User.aggregate([{ $sample: { size: 5 } }]);
+
     const user = await User.create({
         firstName: body?.firstName,
         lastName: body?.lastName,
         email: body?.email,
         image: body?.image
     })
+
+    for (const friend of friends) {
+      console.log(friend._id);
+      await fetch(`${process.env.BASE_URL}/api/users/addFriend`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ userId: user._id, friendId: friend._id })
+      })
+    }
 
     return NextResponse.json({message: "User successfully created", user}, {status: 201});
 
