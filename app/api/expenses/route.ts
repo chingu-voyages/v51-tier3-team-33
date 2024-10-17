d4510961755db7df1aa2c4f5238c988e2e7d5104
Gary Smith <garysmith193358@gmail.com>
2024-10-17 15:31:48 -0400

    Created api route to get all expenses

---
+++ app/api/expenses/route.ts
@@ -0,0 +1,14 @@
+import dbConnect from "@/lib/dbConnect";
+import Expense from "@/models/Expenses";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async(): Promise<NextResponse> => {
+  try {
+    await dbConnect();
+    const expenses = await Expense.find();
+    return NextResponse.json({success: true, expenses}, {status: 200});
+
+  } catch (error) {
+    return NextResponse.json({error: error})
+  }
+}
