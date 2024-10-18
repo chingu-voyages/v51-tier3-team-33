0f879aaeb7924fc50cb46fbfc1419aed94858b7a
Olga Yudkin <cvtqx@protonmail.com>
2024-10-18 09:24:18 -0400

    added sort expenses to recent activity

--- app/(dashboard)/home/ui/recentActivity.tsx
+++ app/(dashboard)/home/ui/recentActivity.tsx
@@ -6,29 +6,32 @@ import { format } from 'date-fns';
 
 export default function RecentActivity() {
   const { userExpenses } = useUserContext();
+
+  //sort expenses from most recent
+  userExpenses.sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
   
-    return (
-      <div className='md:flex-1'>
-        <h2 className='mb-2'>Recent activity:</h2>
-        <ScrollArea className='h-64 w-full rounded-md border bg-muted'>
-          <ul className='p-4'>
-            {userExpenses.map((expense) => (
-              <li key={expense._id}>
-                <p className='text-sm'>
-                  You paid{' '}
-                  <span className='text-purple font-bold'>
-                    {expense.amount}
-                  </span>{' '}
-                  for {expense.name} on{' '}
-                  <span className='text-sm text-gray-400'>
-                    {format(new Date(expense.date), 'MMMM dd, yyyy')}
-                  </span>
-                </p>
-                <Separator className='my-2' />
-              </li>
-            ))}
-          </ul>
-        </ScrollArea>
-      </div>
-    );
+  return (
+    <div className='md:flex-1'>
+      <h2 className='mb-2'>Recent activity:</h2>
+      <ScrollArea className='h-64 w-full rounded-md border bg-muted'>
+        <ul className='p-4'>
+          {userExpenses.map((expense) => (
+            <li key={expense._id}>
+              <p className='text-sm'>
+                You paid{' '}
+                <span className='text-purple font-bold'>{expense.amount}</span>{' '}
+                for {expense.name} on{' '}
+                <span className='text-sm text-gray-400'>
+                  {format(new Date(expense.date), 'MMMM dd, yyyy')}
+                </span>
+              </p>
+              <Separator className='my-2' />
+            </li>
+          ))}
+        </ul>
+      </ScrollArea>
+    </div>
+  );
 }
